1c923e5943117ab60e958c7738c581bcf618f04b
sebitacasa <seba.-14@hotmail.com>
2022-04-28 07:54:24 -0300

    commit

--- api/src/controllers/controllersClients.js
+++ api/src/controllers/controllersClients.js
@@ -22,14 +22,14 @@ const getAllEvent = async (req, res) => {
   }
 };
 const postEvent = async (req, res) => {
-  const { id, title, description, imagen, performers, date, time, stock } = req.body;
+  const {  title, description, imagen, performers, date, time, stock } = req.body;
     
-  if ( !id, !title || !description || !imagen || !performers || !date || !time || !stock) {
+  if ( !title || !description || !imagen || !performers || !date || !time || !stock) {
     return res.status(404).json({ msg: "Info are required" })}
   else{
     try {
       const newEvent = await Event.create({
-        id,
+     
         title,
         description,
         imagen,
--- api/src/models/Event.js
+++ api/src/models/Event.js
@@ -5,7 +5,8 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("Event", {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
+      defaulValue: DataTypes.UUIDV4,
       allowNull: false,
       primaryKey: true,
     },
