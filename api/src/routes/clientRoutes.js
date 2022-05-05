475753a124900d88c131e0f4fde8a26eff900748
unknown <saritagismondi@gmail.com>
2022-05-05 12:11:39 -0300

    datesFiltro

--- api/src/routes/clientRoutes.js
+++ api/src/routes/clientRoutes.js
@@ -1,27 +1,23 @@
 const { Router } = require("express");
 
-const { putEvent, getAllEvent, postEvent, getByTitle, getIdDb, getByState, solocitys, getEventsDb, soloGeneros  } = require("../controllers/controllersClients");
+const { putEvent, postEvent, getByTitle, getIdDb, getByState, solocitys, getEventsDb, soloGeneros, getEventsByDate, datesEvent  } = require("../controllers/controllersClients");
 
 
-
-
-
-
-
+//nueva config de indice de rutas: las de arriba agregue una / al ultimo porfi 
 
 const router = Router();
-router.get("/sologeneros", soloGeneros)
-router.get("/getAll", getEventsDb);
-router.get("/solocitys", solocitys);
-
-router.post("/createEvent", postEvent);
+router.get("/sologeneros/", soloGeneros)
+router.get("/getAll/", getEventsDb);
+router.get("/solocitys/", solocitys);
+router.post("/createEvent/", postEvent);
 router.get("/getTitle", getByTitle);
-router.get("/getStates", getByState);
-router.get("/:id", getIdDb);
-
+router.get("/getStates/", getByState);
+router.get("/getDates/", datesEvent );
+// y las rutas de params agregar luego de esta linea en orden descendente de entradas (como esta ahora) 
 
 router.put("/putEvent/:id", putEvent);
-
+router.get("/getEvents/:date", getEventsByDate);
+router.get("/:id", getIdDb);
 
 
 module.exports = router;
