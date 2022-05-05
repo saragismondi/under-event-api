c4da9450afc186c3a69cb326a46ac58542c59f06
sebitacasa <seba.-14@hotmail.com>
2022-05-05 00:57:55 -0300

    mis cambios

--- api/src/routes/clientRoutes.js
+++ api/src/routes/clientRoutes.js
@@ -1,9 +1,5 @@
 const { Router } = require("express");
-<<<<<<< HEAD
-const { getAllEvent, postEvent, getByTitle, getIdDb, getByState } = require("../controllers/controllersClients");
 const {payment} = require('../controllers/Stripe')
-=======
-
 const {
   getAllEvent,
   postEvent,
@@ -13,7 +9,10 @@ const {
   solocitys,
   putEvent,
 } = require("../controllers/controllersClients");
->>>>>>> dev
+
+
+
+
 
 const router = Router();
 
@@ -23,13 +22,12 @@ router.get("/getTitle", getByTitle);
 router.get("/getStates", getByState);
 router.get("/:id", getIdDb);
 
-<<<<<<< HEAD
 
 router.post("/payment", payment )
-=======
+
 router.put("/putEvent/:id", putEvent);
 
 router.get("/getCities", solocitys);
->>>>>>> dev
+
 
 module.exports = router;
