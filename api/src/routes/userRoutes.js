3430e0c570c219677cfebedc686eacfb72254b00
Lulubonucci <bonucci64@gmail.com>
2022-05-06 15:55:51 +0200

    modelos nuevos, relacion entre tablas

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
 const { postUser } = require("../controllers/controllersUser");
+// const { addCart } = require("../controllers/carrito/addCart");
 
 const router = Router();
 
 router.post("/createUser", postUser);
+// router.post("/card", addCart);
 
 module.exports = router;
