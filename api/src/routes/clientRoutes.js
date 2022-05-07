a195ba734f82e93842a69bc66305b214b6fa9915
sebitacasa <seba.-14@hotmail.com>
2022-05-06 23:24:59 -0300

    stripe

--- api/src/routes/clientRoutes.js
+++ api/src/routes/clientRoutes.js
@@ -1,44 +1,36 @@
 const { Router } = require("express");
-<<<<<<< HEAD
-const {payment} = require('../controllers/Stripe')
+
+const { payment } = require("../controllers/Stripe");
+
 const {
-  getAllEvent,
+  putEvent,
   postEvent,
   getByTitle,
   getIdDb,
   getByState,
   solocitys,
-  putEvent,
+  getEventsDb,
+  soloGeneros,
+  getEventsByDate,
+  datesEvent,
 } = require("../controllers/controllersClients");
-=======
 
-const { putEvent, postEvent, getByTitle, getIdDb, getByState, solocitys, getEventsDb, soloGeneros, getEventsByDate, datesEvent  } = require("../controllers/controllersClients");
-
-
-//nueva config de indice de rutas: las de arriba agregue una / al ultimo porfi 
->>>>>>> dev
-
-
-
-
+//nueva config de indice de rutas: las de arriba agregue una / al ultimo porfi
 
 const router = Router();
-router.get("/sologeneros/", soloGeneros)
+router.get("/sologeneros/", soloGeneros);
 router.get("/getAll/", getEventsDb);
 router.get("/solocitys/", solocitys);
 router.post("/createEvent/", postEvent);
 router.get("/getTitle", getByTitle);
 router.get("/getStates/", getByState);
-router.get("/getDates/", datesEvent );
-// y las rutas de params agregar luego de esta linea en orden descendente de entradas (como esta ahora) 
+router.get("/getDates/", datesEvent);
+// y las rutas de params agregar luego de esta linea en orden descendente de entradas (como esta ahora)
 
-
-router.post("/payment", payment )
+router.post("/payment", payment);
 
 router.put("/putEvent/:id", putEvent);
 router.get("/getEvents/:date", getEventsByDate);
 router.get("/:id", getIdDb);
-
-
 
 module.exports = router;
