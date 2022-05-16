ec02952053291d90e6a4aeb554384160853ae39a
sebitacasa <seba.-14@hotmail.com>
2022-05-16 16:44:24 -0300

    cambios

--- api/src/controllers/Stripe.js
+++ api/src/controllers/Stripe.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const Stripe = require("stripe")(process.env.SECRET_KEY);
 const { User, Order, Ticket, Event } = require("../db");
 const { sendEmail } = require("./sendEmail/sendEmail");
@@ -20,48 +19,37 @@ const mailFail = {
 const payment = async (req, res) => {
   let status, error;
   const { token, amount, orderId } = req.body; // aca tengo que poner el orderId
+  console.log(token, "hola soy el TOKEN")
   try {
     await Stripe.charges.create({
       source: token.id,
       amount: amount * 100,
-      currency: "ARS",
+      currency: "usd",
     });
-
+    //encuentro orden POR ID
     const order = await Order.findByPk(orderId);
 
+    //  aca actualizo el estado dela orden
     order.status = "Approved";
     await order.save();
 
+    //ENCUENTRA TICKETS Y ACTUALIZA SU status
     const encuentroTickets = await Ticket.findAll({
       where: {
         OrderId: orderId,
       },
       include: [{ model: Event }],
     });
+    // console.log(encuentroTickets, "soy los tickets")
 
     await encuentroTickets.forEach(async (ticket) => {
       ticket.status = "Vendido";
       await ticket.save();
-
-    }
-    );
-     /// email formato 
-    
-    mailSucces.message = encuentroTickets.map( e => { 
-      
-      console.log(e.Event)
-      return [ "<h3>Evento: </h3>" ,"<h1>" + 
-      e.Event.dataValues.title +
-       "</h1>","Fecha: ", e.Event.dataValues.date, 
-       "Lugar: ", e.Event.dataValues.location, 
-       "Tu entrada: ", e.dataValues.id]}).join("\n") // esto es un enter
-     
-  
-    const  encuentroUsuario = await  User.findByPk(order.UserId)
     });
     mailSucces.message = encuentroTickets
       .map((e) => {
-        return [
+        //console.log(e);
+        return [ // emailFormato({varibles que quiero enviar}), fecha, hora, lugar, nombre del evento
           "<h3>Evento: </h3>",
           "<h1>" + e.Event.dataValues.title + "</h1>",
           "Fecha: ",
@@ -72,19 +60,23 @@ const payment = async (req, res) => {
           e.dataValues.id,
         ];
       })
-      .join("\n");
+      .join("\n"); // esto es un enter
+    //console.log(mailSucces, "soy el mail de exito");
 
     const encuentroUsuario = await User.findByPk(order.UserId);
     const emailDeUsuario = encuentroUsuario.dataValues.email;
 
     sendEmail(emailDeUsuario, mailSucces);
     status = "success";
+    console.log(emailDeUsuario);
   } catch (error) {
     const order = await Order.findByPk(orderId);
 
+    //  aca actualizo el estado dela orden
     order.status = "Rejected";
     await order.save();
 
+    //ENCUENTRA TICKETS Y ACTUALIZA SU status
     const encuentroTickets = await Ticket.findAll({
       where: {
         OrderId: orderId,
@@ -104,5 +96,8 @@ const payment = async (req, res) => {
   }
   res.json({ error, status });
 };
+
+// ESTA RUT SIEMPRE RESPONDE 200 PERO NECESITAMOS VERIFICAR QUE DIGA SUCCES QUE ESTA DENTRO DE DATA
+// CONSOLOGUEAR DESDE EL FRONT EL RESULT
 
 module.exports = { payment };
