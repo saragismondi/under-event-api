358e0d4acf10a4005bddcddc693043b30dbba94c
msh <mshailant@gmail.com>
2022-05-17 10:50:28 -0300

    creacion de evento

--- api/src/controllers/controllersClients.js
+++ api/src/controllers/controllersClients.js
@@ -85,6 +85,8 @@ const postEvent = async (req, res) => {
     externalId,
   } = req.body;
 
+  console.log(req.body);
+
   if (
     !title ||
     !imagen ||
@@ -98,8 +100,10 @@ const postEvent = async (req, res) => {
     !cost ||
     !month ||
     !address ||
-    !location
+    !location ||
+    !externalId
   ) {
+    console.log("error");
     return res.status(404).json({ msg: "Info are required" });
   } else {
     try {
@@ -120,11 +124,13 @@ const postEvent = async (req, res) => {
         location,
       });
       //console.log(newEvent, "SOY EL EVENTOP");
-      //const user = await User.findOne({
-      //where: { externalId: externalId },
-      //});
-      //console.log(user, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-      //await newEvent.addUser(user);
+      const user = await User.findOne({
+        where: { externalId: externalId },
+      });
+      // await newEvent.addUser(user);
+      await newEvent.update({
+        UserId: user.id,
+      });
 
       let ticketArray = [];
       for (var i = 0; i < stock; i++) {
